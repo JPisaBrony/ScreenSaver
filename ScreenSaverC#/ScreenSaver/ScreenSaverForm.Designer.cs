a2d77ca0bfde0ac63d77efd63cfeb8822b8e9919
JPisaBrony <jdpohl789@gmail.com>
2016-10-07 15:59:56 -0500

    + C# version

---
+++ ScreenSaverC#/ScreenSaver/ScreenSaverForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ScreenSaver
+{
+    partial class ScreenSaverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScreenSaverForm));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.CausesValidation = false;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(1990, 1075);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(10, 10);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            // 
+            // ScreenSaverForm
+            // 
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(2000, 1084);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ScreenSaverForm";
+            this.Text = "Thing";
+            this.Load += new System.EventHandler(this.ScreenSaverForm_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ScreenSaverForm_KeyPress);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ScreenSaverForm_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ScreenSaverForm_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ ScreenSaverC#/ScreenSaver/ScreenSaverForm.cs
@@ -0,0 +1,139 @@
+﻿using AxWMPLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ScreenSaver
+{
+    public partial class ScreenSaverForm : Form
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        static extern bool GetClientRect(IntPtr hWnd, out Rectangle lpRect);
+        private Point mouseLocation;
+        private Random rand = new Random();
+        private bool previewMode = false;
+        private string folder = @"C:\Users\JoshP\Downloads\Videos";
+        private string[] files;
+
+        public ScreenSaverForm()
+        {
+            InitializeComponent();
+        }
+
+        public ScreenSaverForm(IntPtr PreviewWndHandle)
+        {
+            InitializeComponent();
+
+            // Set the preview window as the parent of this window
+            SetParent(this.Handle, PreviewWndHandle);
+
+            // Make this a child window so it will close when the parent dialog closes
+            // GWL_STYLE = -16, WS_CHILD = 0x40000000
+            SetWindowLong(this.Handle, -16, new IntPtr(GetWindowLong(this.Handle, -16) | 0x40000000));
+
+            // Place our window inside the parent
+            Rectangle ParentRect;
+            GetClientRect(PreviewWndHandle, out ParentRect);
+            Size = ParentRect.Size;
+            Location = new Point(0, 0);
+
+            previewMode = true;
+        }
+
+        public ScreenSaverForm(Rectangle Bounds)
+        {
+            InitializeComponent();
+            this.Bounds = Bounds;
+        }
+
+        private void ScreenSaverForm_Load(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+            TopMost = true;
+
+            axWindowsMediaPlayer1.settings.setMode("loop", true);
+            axWindowsMediaPlayer1.PlayStateChange += player_PlayStateChange;
+
+            //axWindowsMediaPlayer1.ClickEvent += playerClicked;
+
+            files = Directory.GetFiles(folder);
+            randomVideoFromFolder();
+        }
+
+        private void randomVideoFromFolder()
+        {
+            var url = files[rand.Next(files.Length)];
+            BeginInvoke(new Action(() => {
+                axWindowsMediaPlayer1.URL = url;
+            }));
+        }
+
+        private void player_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            switch (e.newState)
+            {
+                // paused
+                case 2:
+                    Application.Exit();
+                    break;
+                // playing
+                //case 3:
+                //    axWindowsMediaPlayer1.fullScreen = true;
+                //    axWindowsMediaPlayer1.stretchToFit = true;
+                //    break;
+                // media ended
+                case 8:
+                    randomVideoFromFolder();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void playerClicked(Object sender, _WMPOCXEvents_ClickEvent e)
+        {
+            Application.Exit();
+        }
+
+        private void ScreenSaverForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouseLocation.IsEmpty)
+            {
+                // Terminate if mouse is moved a significant distance
+                if (Math.Abs(mouseLocation.X - e.X) > 5 ||
+                    Math.Abs(mouseLocation.Y - e.Y) > 5)
+                    Application.Exit();
+            }
+
+            // Update current mouse location
+            mouseLocation = e.Location;
+        }
+
+        private void ScreenSaverForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ScreenSaverForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!previewMode)
+                Application.Exit();
+        }
+
+    }
+}
